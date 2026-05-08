3337b9826b3a528107a3cd27921c66a35f8604e5
Lopa10ko <justforprojects@mail.ru>
2026-05-08 18:57:23 +0300

    feat: enhance configuration with environment variables and logging setup

--- stock_market_lstm/config.py
+++ stock_market_lstm/config.py
@@ -1,3 +1,15 @@
+import sys
+import os
+from loguru import logger
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
 DATA_SOURCE = "alphavantage"
 TICKER = "AAL"
-ALPHAVANTAGE_API_KEY = "HA6OU8UWQ7RK6LMC"
+ALPHAVANTAGE_API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
+
+def configure_logging(level: str = "INFO") -> None:
+    logger.remove()
+    logger.add(sys.stderr, level=level)
